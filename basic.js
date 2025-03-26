f45e610810a95ae8d34efe51fe0b38df2e189ff1
jaydipjadhav <jaydipdjadhav>
2025-03-26 10:55:48 +0530

    this is basic three js code file

---
+++ basic.js
@@ -0,0 +1,30 @@
+const scene = new THREE.Scene(); 
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+ camera.position.z = 2;
+
+
+//add
+scene.add(camera);
+
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00  } );
+const mesh = new THREE.Mesh(geometry, material); //add
+
+scene.add(mesh);
+
+
+//render on screne
+const canvas = document.getElementById("main");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);//fill height and width
+
+  renderer.render(scene, camera);
+
+
